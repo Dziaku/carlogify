1dcec287747002989bfbe6a9dec3c2369e448c55
Dziaku <kkubik960@gmail.com>
2024-11-12 21:56:56 +0100

    Created Django project and main app

---
+++ carlogify/carlogify/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for carlogify project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carlogify.settings')
+
+application = get_asgi_application()
