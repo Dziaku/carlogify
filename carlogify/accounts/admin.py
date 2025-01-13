d2762af716144e5497c838119bfeb092011f9a15
Dziaku <kkubik960@gmail.com>
2025-01-13 21:23:45 +0100

    Created custom user model in accounts app. Created admin panel for custom user

--- carlogify/accounts/admin.py
+++ carlogify/accounts/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "date_of_birth",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "first_name",
+        "last_name",
+        "date_of_birth",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": (
+            "first_name",
+            "last_name", 
+            "email", 
+            "password", 
+            "date_of_birth")}
+        ),
+        ("Permissions", {"fields": (
+            "is_staff", 
+            "is_active", 
+            "groups", 
+            "user_permissions")}
+        ),
+    )
+    add_fieldsets = (
+        ( None, {"fields": (
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+            "date_of_birth",
+            "is_staff",
+            "is_active",
+            "groups",
+            "user_permissions")}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- carlogify/accounts/models.py
+++ carlogify/accounts/models.py
@@ -1,3 +1,56 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, date_of_birth, **extra_fields):
+        """
+        Create and save a user with the given email, password,
+        and date_of_birth.
+        """
+        if not email:
+            raise ValueError("The email adress is required.")
+        email = self.normalize_email(email)
+        user = self.model(date_of_birth=date_of_birth, email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password, date_of_birth, **extra_fields):
+        """
+        Create and save a superuser with the given email, 
+        password, and date_of_birth. Extra fields are added
+        to indicate that the user is staff, active, and indeed
+        a superuser.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        
+        return self.create_user(email, password, date_of_birth, **extra_fields)
+    
+
+
+class CustomUser(AbstractUser):
+    
+    username = None
+    email = models.EmailField("email adress", unique=True)
+    date_of_birth = models.DateField("Date of Birth", null=True)
+
+    USERNAME_FIELD= "email"
+    REQUIRED_FIELDS = [
+        "first_name",
+        "last_name",
+        "date_of_birth",
+    ]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+    
